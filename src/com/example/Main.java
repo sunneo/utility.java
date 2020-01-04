9cc1c0f1f4284d89a00687fc1230a5743474c455
sunneo <sunneo529@gmail.com>
2020-01-04 20:06:18 +0800

    fix example

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,18 +1,23 @@
 package com.example;
 
+import com.example.sharp.reflection.ReflectionHelper;
+
 public class Main {
-    public static class ExampleClass{
-        public Integer a;
-        public Double b;
-        public static class Inner{
-            public int a2;
-            public double b2;
-        }
-        Inner inner = new Inner();
-    }
-    public static void main(String[] args) {
-	// write your code here
-        ExampleClass example = new ExampleClass();
-        ReflectionHelper reflectionHelper = new ReflectionHelper(example);
-    }
+	public static class ExampleClass {
+		public Integer a;
+		public Double b;
+
+		public static class Inner {
+			public int a2;
+			public double b2;
+		}
+
+		Inner inner = new Inner();
+	}
+
+	public static void main(String[] args) {
+		// write your code here
+		ExampleClass example = new ExampleClass();
+		ReflectionHelper reflectionHelper = new ReflectionHelper(example);
+	}
 }
